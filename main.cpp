b01450a9ff7a6ed040068d6f0803e0ba2d6987e7
Vinay-kumar-singh <60976588+Vinay-kumar-singh@users.noreply.github.com>
2020-02-14 12:12:53 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,57 @@
+/*--------------------
+| if your using dev-c++
+| you need to link
+| -lws2_32
+| to your application
+---------------------*/
+#include <winsock2.h>
+#include <iostream>
+using namespace std;
+char IP[20];
+int start, endp, temp, err, nret;
+SOCKET sock;
+SOCKADDR_IN Info;
+WSADATA wsadata;
+
+int main()
+{
+    err = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    if(err != 0)
+    {
+        cout << "Error with winsock. Will Now Exit." << endl;
+        cin.get();
+        return 0;
+    }
+
+    cout << "Target IP: ";
+    cin>>IP;
+    cout << "Starting Port: ";
+    cin>>start;
+    cout << "End Port: ";
+    cin>>endp;
+
+    cin.ignore();
+
+    cout << endl << endl << "Starting Scan..." << endl << endl;
+
+    temp = start;
+    while(temp < endp)
+    {
+        sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        Info.sin_family = AF_INET;
+        Info.sin_port = htons(start);
+        nret = connect(sock, NULL, NULL);
+        // error is for line above
+        if(nret != SOCKET_ERROR)
+        {
+            cout << "Port " << temp << " - OPEN! " << endl;
+        }
+        temp++;
+        closesocket(sock);
+    }
+    cout << endl << "Finished With Scan..." << endl;
+
+    cin.get();
+    return 0;
+}
